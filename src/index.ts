2b32df439fa7cccce8c274fc5f484f21d2f23190
Hussain Roker <hussainroker@gmail.com>
2023-05-19 00:25:15 +0200

    solved by Hussain Roker, fst commit

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,31 @@ type Dates = {
   endDate: string;
 };
 
+const errorString = "string length is invalid";
+
 const extractDates = (customer: any) => {
   // your code here
+  try {
+    const ListOfPeriods: string[] = customer?.ListOfPeriods.split("|");
+    const result: Dates[] = [];
+    for (const ListOfPeriod of ListOfPeriods) {
+      const dateArray = ListOfPeriod.trim().split("-");
+      const startDate = dateArray[0].trim();
+      const endDate = dateArray[1].trim();
+      if (startDate.length !== 10 || endDate.length !== 10) {
+        return [errorString];
+      };
+      result.push({
+        startDate,
+        endDate
+      });
+    }
+
+    return result;
+  } catch (error) {
+    return [];
+  }
+
 };
 
 export default extractDates;
